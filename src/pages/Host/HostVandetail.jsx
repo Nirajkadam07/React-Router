d033d304d93570b709d21acac93d223044878519
Nirajkadam07 <nirajkadam2312@gmail.com>
2026-04-17 00:54:44 +0530

    learnt and prctised more about nested route, relative paths, the . usage in NavLink, made the nested routes under host page

--- src/pages/Host/HostVandetail.jsx
+++ src/pages/Host/HostVandetail.jsx
@@ -1,3 +1,74 @@
-export default function HostVandetail() {
-  return <h1>Van detail goes here</h1>;
+import React from "react";
+import { useParams, Link, Outlet, NavLink } from "react-router-dom";
+
+export default function HostVanDetail() {
+  const { id } = useParams();
+  const [currentVan, setCurrentVan] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentVan(data.vans[0]));
+  }, []);
+
+  if (!currentVan) {
+    return <h1>Loading...</h1>;
+  }
+
+  const activeStyle = {
+    fontWeight: " bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
+  return (
+    <>
+      <section>
+        <Link to=".." relative="path" className="back-button">
+          &larr; <span>Back to all vans</span>
+        </Link>
+
+        <div className="host-van-detail-layout-container">
+          <div className="host-van-detail">
+            <img src={currentVan.imageUrl} />
+            <div className="host-van-detail-info-text">
+              <i className={`van-type van-type-${currentVan.type}`}>
+                {currentVan.type}
+              </i>
+              <h3>{currentVan.name}</h3>
+              <h4>${currentVan.price}/day</h4>
+            </div>
+          </div>
+        </div>
+      </section>
+      <nav className="host-van-detail-nav">
+        <NavLink
+          to="."
+          style={({ isActive }) => {
+            return isActive ? activeStyle : null;
+          }}
+          end
+        >
+          Details
+        </NavLink>
+        <NavLink
+          to="pricing"
+          style={({ isActive }) => {
+            return isActive ? activeStyle : null;
+          }}
+        >
+          Pricing
+        </NavLink>
+        <NavLink
+          to="photos"
+          style={({ isActive }) => {
+            return isActive ? activeStyle : null;
+          }}
+        >
+          Photos
+        </NavLink>
+      </nav>
+      <Outlet context={{ currentVan }} />
+    </>
+  );
 }
